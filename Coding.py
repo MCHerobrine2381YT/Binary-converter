c5743227232944957538613fe073efd0a0cd28ba
MCHerobrine2381YT <117151962+MCHerobrine2381YT@users.noreply.github.com>
2022-12-14 21:43:23 +0700

    Update Coding.py

--- Coding.py
+++ Coding.py
@@ -1,9 +1,13 @@
+class console:
+    def log(x):
+        print(x)
+
 class Binary:
     def __init__(self, run):
         self.run = bin(run)
-    
+                    
     def convert_binary(self):
-        print("Binary code: {}".format(self.run))
-
+        console.log("Binary code: {}".format(self.run))
+                        
 code = Binary(int(input("Convert to binary: ")))
 code.convert_binary()
